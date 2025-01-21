e20ca60ccc9b3fda548db437b4fdaa2b69186180
Elena <110896958+Elena-web@users.noreply.github.com>
2025-01-21 16:04:58 +0300

    Updates

--- src/components/Header/Header.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const HeaderContainer = styled.header`
-  background-color: #2196f3;
-  color: #ffffff;
-  box-shadow: 0px 4px 4px 0px rgb(0, 0, 0, 0.3);
-`
-
-export default HeaderContainer
